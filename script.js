5b327fb12cbde1c9159cbf30e624018d295caabe
Claudio9616 <clares9611@gmail.com>
2023-12-21 17:15:48 +0100

    emal version 1

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// DADI
 const button = document.getElementById('button')
 const victory = document.getElementById('vincitore')
 button.addEventListener('click', function(){
@@ -9,11 +10,31 @@ button.addEventListener('click', function(){
     resultCpu = Math.floor(Math.random() * 6) + 1
     console.log(resultCpu, 'resultCpu')
     if(resultUser > resultCpu){
-        message = 'HAI VINTO TU....'
+        message = 'HAI VINTO TU'
     } else {
         message = 'HO VINTO IO SFIGATO!'
     }
     victory.innerText = message
+})
+// EMAIL
+const email = ['ciccio.pasticcio@libero.it',
+ 'pasticcio.ciccio@gmail.com',
+  'moana.pozzi@bigbird.it',
+   'martina.smeraldi@verybigbird.it']
+const emailUser = document.getElementById('email')
+const emailButton = document.getElementById('mail-button')
+const text = document.getElementById('text')
+
+emailButton.addEventListener('click', function(){
+const emailDichiarata = emailUser.value
+console.log(emailDichiarata)
+let message1 = ''
+if(email === emailDichiarata){
+    message1 = 'puoi accedere'   
+} else{
+    message1 = 'INTRUSO!'
+}
+text.innerText = message1
 })
 
 
